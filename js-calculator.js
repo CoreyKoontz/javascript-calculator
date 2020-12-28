5a3a6914aa169f5e0b87e1698a23bb3a269bdc09
Corey Koontz <corey.michael.koontz@gmail.com>
2020-12-27 23:58:46 -0600

    feat: adding modifications to current js code for js calculator

--- js-calculator.js
+++ js-calculator.js
@@ -22,13 +22,34 @@ const calculator = {
     operator: null, // -Stores the operator for an expression.
 };
 
+//******* Input the Digits *****************************************************
+
+// (?) ternary operator is used to check if the current value displayed on the calculator is 0.
+// If so, calculator.displayValue is overwritten with whatever digit was clicked. Otherwise, if
+// displayValue is a non-zero number, the digit is appended to it through string concatenation.
+
+function inputDigit(digit) {
+    const {displayValue } = calculator;
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+}
+//******* Input a Decimal Point ************************************************
+
+function inputDecimal(dot) {
+    // If the `displayValue` property does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+        // Append the decimal point
+        calculator.displayValue += dot; // (+=) adds the value of the right operand to a
+                                        // variable and assigns the result to the variable.
+    }
+}
+
 //******* Updating the Display *************************************************
 
 // -We need the contents of displayValue to be shown at all times. So we
 //  must create a function that updates the screen with the values of displayValue
 //  every time an operation is performed.
 // - document.querySelector(selectors); -- The Document method querySelector() returns
-//   the first Element within the document that matches the specified selector,
+//   the first Element within the document (html page) that matches the specified selector,
 //   or group of selectors. If no matches are found, null is returned.
 
 function updateDisplay() {
@@ -44,7 +65,37 @@ updateDisplay();
 
 // -The four sets of keys on the calculator are: digits (1-9), operators (+, -, *, /, =),
 //  the decimal point(.) and a reset key (AC).
+// -arrow functions (=>) allow for shorter function syntax
+// -.addEventListener -The EventTarget method addEventListener() sets up a function
+//  that will be called whenever the specified event is delivered to the target.
 
 const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) =>{})
+keys.addEventListener('click', (event) => {
+    // Listening for a click event
+    // Access the clicked element
+    const { target } = event; // This is the callback function of the event listener.
+    // The target variable is an object that represents the element that was clicked.
+    // Check if the clicked element is a button.
+    // If not, exit from the function
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value);
+        return;
+    }
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
 
